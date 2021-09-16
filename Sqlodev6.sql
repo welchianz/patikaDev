7785bf364deb4b12732d7ed45034b3526d5df170
welchianz <80834085+welchianz@users.noreply.github.com>
2021-09-17 00:52:06 +0300

    Update Sqlodev6.sql

--- Sqlodev6.sql
+++ Sqlodev6.sql
@@ -1,4 +1,5 @@
 /*
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 1.film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
 2.film tablosunda bulunan filmlerden kaçtanesi 'C' karekteri ile başlar?
 3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
