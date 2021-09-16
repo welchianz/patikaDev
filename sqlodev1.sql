b51651843e7bc5f830ac9b087a6ca7678ae9cb12
welchianz <80834085+welchianz@users.noreply.github.com>
2021-09-16 21:11:36 +0300

    Update and rename sqlproje1.sql to sqlodev1.sql

---
+++ sqlodev1.sql
@@ -0,0 +1,10 @@
+--SORU 1
+SELECT title, description FROM film;
+--SORU 2
+SELECT * FROM film WHERE length > 60 AND length < 75;
+--SORU 3
+SELECT rental_rate, replacement_cost FROM film WHERE rental_rate = 0.99 AND (replacement_cost=12.99 OR replacement_cost=28.99);
+--SORU 4
+SELECT first_name, last_name FROM customer WHERE first_name='Mary';
+--SORU 5
+SELECT * FROM film  WHERE length<50 AND (rental_rate!=2.99 OR rental_rate!=4.99)
