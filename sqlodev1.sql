a97038523a12dc948107b78e59567fdfe5976aaf
welchianz <80834085+welchianz@users.noreply.github.com>
2021-09-17 00:51:46 +0300

    Update sqlodev1.sql

--- sqlodev1.sql
+++ sqlodev1.sql
@@ -1,4 +1,5 @@
 /*
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
 1.film tablosunda bulunan title ve description sütunlarındaki verileri sıralayınız.
 2.film tablosunda bulunan tüm sütunlardaki verileri film uzunluğu (length) 60 dan büyük VE 75 ten küçük olma koşullarıyla sıralayınız.
 3.film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99 VE replacement_cost 12.99 VEYA 28.99 olma koşullarıyla sıralayınız.
