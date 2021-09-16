b18f32bcc2d6465f6112a60985d22189547ffe2a
welchianz <80834085+welchianz@users.noreply.github.com>
2021-09-17 00:47:36 +0300

    Create Sqlodev6.sql

---
+++ Sqlodev6.sql
@@ -0,0 +1,14 @@
+/*
+1.film tablosunda bulunan rental_rate sütunundaki değerlerin ortalaması nedir?
+2.film tablosunda bulunan filmlerden kaçtanesi 'C' karekteri ile başlar?
+3.film tablosunda bulunan filmlerden rental_rate değeri 0.99 a eşit olan en uzun (length) film kaç dakikadır?
+4.film tablosunda bulunan filmlerin uzunluğu 150 dakikadan büyük olanlarına ait kaç farklı replacement_cost değeri vardır?
+*/
+--SORU 1
+SELECT AVG(rental_rate) FROM film;
+--SORU 2
+SELECT COUNT(film) FROM film WHERE film LIKE 'C%';
+--SORU 3
+SELECT MAX(length) FROM film WHERE rental_rate = 0.99;
+--SORU 4
+SELECT COUNT(DISTINCTreplacement_cost) FROM film WHERE length > 150;
